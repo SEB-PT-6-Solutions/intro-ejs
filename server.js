d3969768ef831716063db5fd88053f28d94d94bf
Denis S Dujota <denisdujota@gmail.com>
2025-08-30 13:26:27 +0300

    finish setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = 3000;
+const app = express();
+// MIDDLEWARE
+app.use(morgan('dev'));
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('The server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port 3000');
+});
